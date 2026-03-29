68f0342b687d9a94f74fcc205cc5f71815dc6df0
daqbang-dot <daqbang@gmail.com>
2026-03-30 02:11:28 +0800

    Update user_registry.js

--- src/core/user_registry.js
+++ src/core/user_registry.js
@@ -1,9 +1,23 @@
 export const userRegistry = [
     {
+        username: "ceo", 
+        password: "ceo123", // Kunci masuk CEO
+        id: "CEO-01",
+        name: "AzlanMymo",
+        role: "ceo",
+        pin: "8888",
+        photo: "https://i.pravatar.cc/150?u=ceo",
+        contractStatus: "Owner",
+        joinDate: "2026-01-01",
+        accessLevel: "Full Access"
+    },
+    {
+        username: "staff01",
+        password: "user123", // Kunci masuk Staff
         id: "OP-77",
         name: "Operator Alpha",
         role: "operator",
-        pin: "1234", // Pin untuk transaksi sensitif
+        pin: "1234",
         photo: "https://i.pravatar.cc/150?u=op77",
         contractStatus: "Active",
         joinDate: "2026-01-15",
@@ -11,6 +25,8 @@ export const userRegistry = [
         accessLevel: "Quarter Access"
     },
     {
+        username: "exec01",
+        password: "pass123", // Kunci masuk Exec
         id: "EX-02",
         name: "Architect Beta",
         role: "executive",
