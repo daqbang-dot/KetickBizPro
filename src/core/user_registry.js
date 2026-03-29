9e2a0a907c860299707a210ba4ba3ed0052c918b
daqbang-dot <daqbang@gmail.com>
2026-03-30 01:33:28 +0800

    Create user_registry.js

---
+++ src/core/user_registry.js
@@ -0,0 +1,24 @@
+export const userRegistry = [
+    {
+        id: "OP-77",
+        name: "Operator Alpha",
+        role: "operator",
+        pin: "1234", // Pin untuk transaksi sensitif
+        photo: "https://i.pravatar.cc/150?u=op77",
+        contractStatus: "Active",
+        joinDate: "2026-01-15",
+        contractFile: "contract_alpha.pdf",
+        accessLevel: "Quarter Access"
+    },
+    {
+        id: "EX-02",
+        name: "Architect Beta",
+        role: "executive",
+        pin: "8899",
+        photo: "https://i.pravatar.cc/150?u=ex02",
+        contractStatus: "Probation",
+        joinDate: "2026-02-01",
+        contractFile: "contract_beta.pdf",
+        accessLevel: "Half Access"
+    }
+];
